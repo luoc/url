b62ae7cbe4a5de03738ac1e44b5f747006f0df84
luocheng <starmate@126.com>
2013-12-08 17:45:53 +0800

    initial commit
    just generate spider from crawl templete

---
+++ url/spiders/urlman.py
@@ -0,0 +1,21 @@
+from scrapy.selector import HtmlXPathSelector
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from url.items import UrlItem
+
+class UrlmanSpider(CrawlSpider):
+    name = 'urlman'
+    allowed_domains = ['sina.com.cn']
+    start_urls = ['http://www.sina.com.cn/']
+
+    rules = (
+        Rule(SgmlLinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+    )
+
+    def parse_item(self, response):
+        hxs = HtmlXPathSelector(response)
+        i = UrlItem()
+        #i['domain_id'] = hxs.select('//input[@id="sid"]/@value').extract()
+        #i['name'] = hxs.select('//div[@id="name"]').extract()
+        #i['description'] = hxs.select('//div[@id="description"]').extract()
+        return i
